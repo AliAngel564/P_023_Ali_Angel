c1bd76fad408c46ff56059fb0c84db4535c0c43e
AliAngel564 <jonathan.aliangel@gmail.com>
2025-04-22 21:43:30 -0600

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,120 @@
+
+#include <iostream>
+#include <conio.h>
+
+void pressAnyKey();
+
+class Bank
+{
+    private:
+    std::string propietary;
+    std::string acctType;
+    double balance;
+    bool savings = false;
+    bool checking = false;
+    bool payroll = false;
+
+   
+
+    public:
+    void setPropietary(std::string name)
+    {
+        propietary = name;
+    }
+    std::string getPropietary()
+    {
+        return propietary;
+    }
+    void setAcctToSavings()
+    {
+        acctType = "Savings";
+        savings = true;
+    }
+    void setAcctToChecking()
+    {
+        acctType = "Checking";
+        checking = true;
+    }
+    void setAcctToPayroll()
+    {
+        acctType = "Payroll";
+        payroll = true;
+    }
+    std::string getAcctType()
+    {
+        return acctType;
+    }
+    double getBalance()
+    {
+        return balance;
+    }
+    void setBalance(double usrBalance)
+    {
+        balance = usrBalance;
+    }
+    void makeDeposit(double depositAmount)
+    {
+        std::cout<<"You have deposited: $" << depositAmount << " to your " << getAcctType() << " account\n";
+        balance = balance + depositAmount;
+        consultBalance();
+    }
+    void makeWithdrawal(double withdrawalAmount)
+    {
+        if(balance-withdrawalAmount < 0 && checking !=true)
+        {
+            std::cout<<"Withdrawal exceeds available money in account, try with a different sum\n";
+            consultBalance();
+        }else if(savings == true || payroll == true)
+            {
+                std::cout<<"You have whitdrawn: $" << withdrawalAmount << " from your " << getAcctType() << " account\n";
+                balance = balance - withdrawalAmount;
+                consultBalance();
+            }else
+            {
+                std::cout<<"You can't make a withdrawal on a Checking account\n";
+                pressAnyKey();
+            }
+    }
+    void consultBalance()
+    {
+        std::cout<<"The current balance in your "<< getAcctType() << " account is: $" << getBalance()<<"\n";
+        pressAnyKey();
+    }
+    void getAcctInfo()
+    {
+        std::cout<<"This " << getAcctType() <<" account is registered under: " << getPropietary();
+        std::cout<<"\n";
+        pressAnyKey();
+    }
+};
+
+
+int main()
+{
+    Bank firstAccount;
+    Bank secondAccount;
+    firstAccount.setPropietary("Juan Manuel Vicente");
+    firstAccount.setBalance(0);
+    firstAccount.setAcctToChecking();
+    firstAccount.makeDeposit(1200);
+    firstAccount.makeWithdrawal(500);
+    firstAccount.makeDeposit(200);
+    firstAccount.makeWithdrawal(5000);
+    firstAccount.getAcctInfo();
+    pressAnyKey();
+    secondAccount.setPropietary("Pedro Esteban Lopez");
+    secondAccount.setBalance(0);
+    secondAccount.setAcctToPayroll();
+    secondAccount.makeDeposit(1200);
+    secondAccount.makeWithdrawal(500);
+    secondAccount.makeDeposit(200);
+    secondAccount.makeWithdrawal(5000);
+    secondAccount.getAcctInfo();
+}
+
+void pressAnyKey()
+{
+    std::cout<<"PRESS ANY KEY TO CONTINUE";
+    getch();
+    system("cls");
+}
